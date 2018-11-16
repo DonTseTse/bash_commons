2a0b554559f6bb3cc898e85cca6e14e6e052ac86
DonTseTse <dontsetse@IntraNUC>
2018-11-16 11:15:43 +0100

    Added script path handling

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # bash_commons
 Collection of bash functions for common tasks
+
+# Snippets
+
+Script directory resolution (f.ex. in an installer before `bash_commons` are available)
+```bash
+# This snippet refuses file symlinks (folders are OK) and sets script_folder to the directory the script lies in
+if [ -h "${BASH_SOURCE[0]}" ]; then echo "Please call ... directly. The call through a symlink gives the wrong working directory. Aborting..."; exit 1; fi
+script_folder="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
+```
+**Important**: this code has to run before files are sourced, subshells are launched etc. because such operations affect `$BASH_SOURCE` (`get_script_path()` 
+               from [filesystem.sh](filesystem.sh) is able to cope with that and file symlinks)
--- filesystem.sh
+++ filesystem.sh
@@ -1,5 +1,29 @@
 #! /bin/bash
 
+### get_script_path
+# Rewritten from https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within/246128#246128
+# Unlike the SO answer, this function returns the full path including the filename and it's able to work in any call
+# constellation: sourced, called in a subshell etc. - all things that affect $BASH_SOURCE, but one thing is guaranteed: the element
+# in this array with the highest index is the originally called script
+#
+# Returns - status: always 0
+#         - stdout: "real" (symlink resolved) absolute path of the executed script
+function get_script_path()
+{
+	#use source with highest index (see explanations above)
+	local src="${BASH_SOURCE[((${#BASH_SOURCE[@]}-1))]}"
+	local dir
+	# resolve eventual symlinks in $src
+	while [ -h "$src" ]; do
+  		src="$(readlink "$src")"
+		# if $src is a relative symlink, the path needs to be treated with respect to the symlink's location
+  		if [[ $src != /* ]]; then
+			src="$(cd -P "$(dirname "$src")" >/dev/null && pwd)/$src"
+		fi
+	done
+	echo "$(cd -P "$(dirname "$src")" >/dev/null && pwd)/$(basename "$src")"
+}
+
 ### try_filepath_deduction
 # If there's only a single file (match) in the folder $1, returns it
 #
